3634382fabaf53b3178ea1baebb8d0b25e3e1391
Amroshwayat <amrshwayat85@gmail.com>
2025-09-19 14:33:48 +0300

    Task44

---
+++ WebApplication1/Controllers/GreeterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreeterController : ControllerBase
+    {
+     
+        [HttpGet]
+        public IActionResult Get([FromQuery] string? name)
+        {
+        
+            var personName = string.IsNullOrWhiteSpace(name) ? "anonymous" : name;
+
+        
+            return Ok($"Hello {personName}");
+        }
+    }
+}
